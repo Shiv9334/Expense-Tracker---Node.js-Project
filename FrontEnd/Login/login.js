8eec5fb979668cbaaec0d45280c57b60245dc9eb
Shiv <shivkumar9334@gmail.com>
2023-03-27 12:34:53 +0530

    task-4 Creating Login Screen Frontend

---
+++ FrontEnd/Login/login.js
@@ -0,0 +1,29 @@
+const myForm = document.querySelector(".login-form");
+const msg = document.querySelector(".msg");
+const Email = document.querySelector("#email");
+const Password = document.querySelector("#password");
+let url = "http://localhost:4000/user/login";
+
+myForm.addEventListener("submit", onSubmit);
+
+async function onSubmit(e) {
+  e.preventDefault();
+  try {
+    const user = {
+      email: Email.value,
+      password: Password.value,
+    };
+    const response = await axios.post(url, user);
+    console.log(response);
+    if (response === 200) {
+      alert("LogIn Successful!!!");
+    } else {
+      throw new Error("Failed to login");
+    }
+  } catch (err) {
+    msg.classList.add("warning");
+    alert("Something went Wrong");
+    msg.textContent = err.response.data.error;
+    setTimeout(() => msg.remove(), 3000);
+  }
+}
