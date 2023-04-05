90b62a3f769a3c2828e5457066b8c9ac5516142e
Shiv <shivkumar9334@gmail.com>
2023-04-05 15:24:50 +0530

    task=17 Backend (Premium Feature)

--- Backend/controller/premium.js
+++ Backend/controller/premium.js
@@ -1,7 +1,9 @@
 const User = require("../models/user");
-const Expense = require("../models/expense");
+const UserExpense = require("../models/expense");
 const sequelize = require("../util/database");
-const express = require("express");
+const S3Services = require("../services/s3services");
+const AWS = require("aws-sdk");
+const FileDb = require("../models/download");
 
 async function getAllUsersWithExpenses(req, res) {
   try {
@@ -15,4 +17,37 @@ async function getAllUsersWithExpenses(req, res) {
   }
 }
 
-module.exports = { getAllUsersWithExpenses };
+async function download(req, res, next) {
+  try {
+    const userId = req.user.id;
+    const expense = await UserExpense.findAll({ where: { userId: userId } });
+    const strExpenses = JSON.stringify(expense);
+    const filename = `Expense${userId}/${new Date()}.txt`;
+    const fileURL = await S3Services.uploadToS3(strExpenses, filename);
+    const link = await FileDb.create({
+      fileURL: fileURL,
+      userId: userId,
+    });
+    res.status(201).json({ fileURL, success: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+}
+
+async function downloadFile(req, res, next) {
+  try {
+    const userId = req.user.id;
+    const links = await FileDb.findAll({
+      where: {
+        userId: userId,
+      },
+    });
+
+    res.json(links);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+module.exports = { getAllUsersWithExpenses, download, downloadFile };
